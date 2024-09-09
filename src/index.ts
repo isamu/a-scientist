7a0bbeae1356e18654cc3576472abf632050e82e
isamu <isamu@to-kyo.to>
2024-09-09 20:10:05 +0900

    ai

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,170 @@
-console.log("HELLO");
+import fs from "fs";
+import path from "path";
+
+import "dotenv/config";
+
+import { idea_first_prompt, idea_reflection_prompt } from "./text";
+
+import { GraphAI } from "graphai";
+import { openAIAgent } from "@graphai/openai_agent";
+import * as vanilla_agent from "@graphai/vanilla";
+import arrayFlatAgent from "./array_flat_agent";
+
+const agents = { openAIAgent, ...vanilla_agent, arrayFlatAgent };
+
+const getBaseDir = (name: string) => {
+  return path.resolve(__dirname, "../templates/" + name);
+};
+
+const loadJsonFile = (fileName: string) => {
+  const fileData = fs.readFileSync(fileName, "utf8");
+  const text = JSON.parse(fileData);
+  return text;
+};
+
+const formatString = (text: string, dataSet: Record<string, string>) => {
+  return Object.entries(dataSet).reduce((tmp, [key, value]) => {
+    return tmp.replaceAll("{" + key + "}", value);
+  }, text);
+};
+
+// const load
+
+// mock
+const get_response_from_llm = (message: string, system_message: string, llm: string, model: string, message_history: string[] = []) => {
+  const text = "";
+  const msg_history: string[] = [];
+
+  return [text, msg_history];
+};
+
+const generate_ideas = async (baseDir: string, skipGeneration = false, maxNumGenerations = 20, num_reflections = 5) => {
+  if (skipGeneration) {
+    const ideas = loadJsonFile(baseDir + "/ideas.json");
+    console.log(ideas);
+    return ideas;
+  }
+
+  const seedIdeas = loadJsonFile(baseDir + "/seed_ideas.json");
+  const ideaStrArchive = seedIdeas.map((m: unknown) => JSON.stringify(m));
+  const code = fs.readFileSync(baseDir + "/experiment.py", "utf8");
+  const prompt = loadJsonFile(baseDir + "/prompt.json");
+
+  const idea_system_prompt = prompt["system"];
+
+  try {
+    const prev_ideas_string = ideaStrArchive.join("\n\n");
+
+    console.log(`Iteration 1/${num_reflections}`);
+    const message = formatString(idea_first_prompt, {
+      task_description: prompt["task_description"],
+      code,
+      prev_ideas_string,
+      num_reflections: String(num_reflections),
+    });
+    // console.log(message);
+
+    const graph_data = {
+      version: 0.5,
+      loop: {
+        count: 2,
+      },
+      nodes: {
+        message: {
+          value: message,
+        },
+        history: {
+          value: [],
+          update: ":nextHistory.array",
+        },
+        task1: {
+          agent: "openAIAgent",
+          params: {
+            prompt: message,
+            system: idea_system_prompt,
+            messages: ":history",
+          },
+          isResult: true,
+        },
+        jsonParse: {
+          agent: "jsonParserAgent",
+          inputs: [":task1.choices.$0.message.content"],
+          isResult: true,
+        },
+        messageData: {
+          agent: "stringTemplateAgent",
+          inputs: [":message", ":task1.choices.$0.message.content"],
+          params: {
+            template: [
+              { role: "user", content: "${0}" },
+              { role: "assistant", content: "${1}" },
+            ],
+          },
+        },
+        nextHistory: {
+          agent: "arrayFlatAgent",
+          inputs: {
+            array: [":history", ":messageData"],
+          },
+        },
+        improveTask: {
+          agent: "nestedAgent",
+          inputs: { prevHistory: ":nextHistory.array" },
+          isResult: true,
+          graph: {
+            version: 0.5,
+            loop: {
+              count: 2,
+            },
+            history: {
+              value: ":prevHistory",
+              update: ":nextHistory.array",
+            },
+            nodes: {
+              task2: {
+                agent: "openAIAgent",
+                params: {
+                  prompt: idea_reflection_prompt,
+                  system: idea_system_prompt,
+                  messages: ":history",
+                },
+              },
+            },
+            messageData: {
+              agent: "stringTemplateAgent",
+              inputs: [":message", ":task2.choices.$0.message.content"],
+              params: {
+                template: [
+                  { role: "user", content: "${0}" },
+                  { role: "assistant", content: "${1}" },
+                ],
+              },
+            },
+            nextHistory: {
+              agent: "arrayFlatAgent",
+              inputs: {
+                array: [":history", ":messageData"],
+              },
+              isResult: true,
+            },
+          },
+        },
+      },
+    };
+    const graph = new GraphAI(graph_data, agents);
+    const result = (await graph.run()) as any;
+    console.log(result);
+
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const main = async () => {
+  const experiment = "grokking";
+
+  const baseDir = getBaseDir(experiment);
+  await generate_ideas(baseDir);
+};
+
+main();
---
+++ src/openai_agent.ts
@@ -0,0 +1,268 @@
+import OpenAI from "openai";
+import { AgentFunction, AgentFunctionInfo, sleep } from "graphai";
+import { GrapAILLMInputBase, getMergeValue } from "@graphai/llm_utils";
+
+type OpenAIInputs = {
+  model?: string;
+  images?: string[];
+  tools?: OpenAI.ChatCompletionTool[];
+  tool_choice?: OpenAI.ChatCompletionToolChoiceOption;
+  max_tokens?: number;
+  verbose?: boolean;
+  temperature?: number;
+  baseURL?: string;
+  apiKey?: string;
+  stream?: boolean;
+  messages?: Array<Record<string, any>>;
+  forWeb?: boolean;
+} & GrapAILLMInputBase;
+
+export const openAIAgent: AgentFunction<OpenAIInputs, Record<string, any> | string, string | Array<any>, OpenAIInputs> = async ({
+  filterParams,
+  params,
+  namedInputs,
+}) => {
+  const { verbose, system, images, temperature, tools, tool_choice, max_tokens, baseURL, apiKey, stream, prompt, messages, forWeb } = {
+    ...params,
+    ...namedInputs,
+  };
+
+  const userPrompt = getMergeValue(namedInputs, params, "mergeablePrompts", prompt);
+  const systemPrompt = getMergeValue(namedInputs, params, "mergeableSystem", system);
+
+  // Notice that we ignore params.system if previous_message exists.
+  const messagesCopy: Array<any> = [...(systemPrompt ? [{ role: "system", content: systemPrompt }] : []), ...(messages ?? []).map((m) => m)];
+
+  if (userPrompt) {
+    messagesCopy.push({
+      role: "user",
+      content: userPrompt,
+    });
+  }
+  if (images) {
+    const image_url =
+      params.model === "gpt-4-vision-preview"
+        ? images[0]
+        : {
+            url: images[0],
+            detail: "high",
+          };
+    messagesCopy.push({
+      role: "user",
+      content: [
+        {
+          type: "image_url",
+          image_url,
+        },
+      ],
+    });
+  }
+
+  if (verbose) {
+    console.log(messagesCopy);
+  }
+
+  const openai = new OpenAI({ apiKey, baseURL, dangerouslyAllowBrowser: !!forWeb });
+
+  const chatParams = {
+    model: params.model || "gpt-3.5-turbo",
+    messages: messagesCopy,
+    tools,
+    tool_choice,
+    max_tokens,
+    temperature: temperature ?? 0.7,
+  };
+  if (!stream) {
+    return await openai.chat.completions.create(chatParams);
+  }
+  const chatStream = openai.beta.chat.completions.stream({
+    ...chatParams,
+    stream: true,
+  });
+
+  for await (const message of chatStream) {
+    const token = message.choices[0].delta.content;
+    if (filterParams && filterParams.streamTokenCallback && token) {
+      filterParams.streamTokenCallback(token);
+    }
+  }
+
+  const chatCompletion = await chatStream.finalChatCompletion();
+  return chatCompletion;
+};
+
+const input_sample = "this is response result";
+const result_sample = {
+  object: "chat.completion",
+  id: "chatcmpl-9N7HxXYbwjmdbdiQE94MHoVluQhyt",
+  choices: [
+    {
+      message: {
+        role: "assistant",
+        content: input_sample,
+      },
+      finish_reason: "stop",
+      index: 0,
+      logprobs: null,
+    },
+  ],
+  created: 1715296589,
+  model: "gpt-3.5-turbo-0125",
+};
+
+export const openAIMockAgent: AgentFunction<
+  {
+    model?: string;
+    query?: string;
+    system?: string;
+    verbose?: boolean;
+    temperature?: number;
+  },
+  Record<string, any> | string,
+  string | Array<any>
+> = async ({ filterParams }) => {
+  for await (const token of input_sample.split("")) {
+    if (filterParams && filterParams.streamTokenCallback && token) {
+      await sleep(100);
+      filterParams.streamTokenCallback(token);
+    }
+  }
+
+  return result_sample;
+};
+const openaiAgentInfo: AgentFunctionInfo = {
+  name: "openAIAgent",
+  agent: openAIAgent,
+  mock: openAIMockAgent,
+  inputs: {
+    type: "object",
+    properties: {
+      model: { type: "string" },
+      system: { type: "string" },
+      tools: { type: "object" },
+      tool_choice: {
+        anyOf: [{ type: "array" }, { type: "object" }],
+      },
+      max_tokens: { type: "number" },
+      verbose: { type: "boolean" },
+      temperature: { type: "number" },
+      baseURL: { type: "string" },
+      apiKey: {
+        anyOf: [{ type: "string" }, { type: "object" }],
+      },
+      stream: { type: "boolean" },
+      prompt: {
+        type: "string",
+        description: "query string",
+      },
+      messages: {
+        anyOf: [{ type: "string" }, { type: "object" }, { type: "array" }],
+        description: "chat messages",
+      },
+    },
+  },
+  output: {
+    type: "object",
+    properties: {
+      id: {
+        type: "string",
+      },
+      object: {
+        type: "string",
+      },
+      created: {
+        type: "integer",
+      },
+      model: {
+        type: "string",
+      },
+      choices: {
+        type: "array",
+        items: [
+          {
+            type: "object",
+            properties: {
+              index: {
+                type: "integer",
+              },
+              message: {
+                type: "array",
+                items: [
+                  {
+                    type: "object",
+                    properties: {
+                      content: {
+                        type: "string",
+                      },
+                      role: {
+                        type: "string",
+                      },
+                    },
+                    required: ["content", "role"],
+                  },
+                ],
+              },
+            },
+            required: ["index", "message", "logprobs", "finish_reason"],
+          },
+        ],
+      },
+      usage: {
+        type: "object",
+        properties: {
+          prompt_tokens: {
+            type: "integer",
+          },
+          completion_tokens: {
+            type: "integer",
+          },
+          total_tokens: {
+            type: "integer",
+          },
+        },
+        required: ["prompt_tokens", "completion_tokens", "total_tokens"],
+      },
+    },
+    required: ["id", "object", "created", "model", "choices", "usage"],
+  },
+  params: {
+    type: "object",
+    properties: {
+      model: { type: "string" },
+      system: { type: "string" },
+      tools: { type: "object" },
+      tool_choice: { anyOf: [{ type: "array" }, { type: "object" }] },
+      max_tokens: { type: "number" },
+      verbose: { type: "boolean" },
+      temperature: { type: "number" },
+      baseURL: { type: "string" },
+      apiKey: { anyOf: [{ type: "string" }, { type: "object" }] },
+      stream: { type: "boolean" },
+      prompt: { type: "string", description: "query string" },
+      messages: { anyOf: [{ type: "string" }, { type: "object" }, { type: "array" }], description: "chat messages" },
+    },
+  },
+  outputFormat: {
+    llmResponse: {
+      key: "choices.$0.message.content",
+      type: "string",
+    },
+  },
+  samples: [
+    {
+      inputs: { prompt: input_sample },
+      params: {},
+      result: result_sample,
+    },
+  ],
+  description: "OpenAI Agent",
+  category: ["llm"],
+  author: "Receptron team",
+  repository: "https://github.com/receptron/graphai",
+  license: "MIT",
+  stream: true,
+  npms: ["openai"],
+  environmentVariables: ["OPENAI_API_KEY"],
+};
+
+export default openaiAgentInfo;
