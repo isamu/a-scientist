105d74bfab1aff13d11f462f391d2a696f80a269
isamu <isamu@to-kyo.to>
2024-09-11 15:22:26 +0900

    add stringTemplateAgent

---
+++ src/string_template_agent.ts
@@ -0,0 +1,106 @@
+import { AgentFunction, AgentFunctionInfo } from "graphai";
+
+type StringTemplate = string | Record<string, string>;
+type StringTemplateObject = StringTemplate | StringTemplate[] | Record<string, StringTemplate>;
+
+const processTemplate: any = (template: StringTemplateObject, match: string, input: string) => {
+  if (typeof template === "string") {
+    return template.replace(match, input);
+  } else if (Array.isArray(template)) {
+    return template.map((item: StringTemplate) => processTemplate(item, match, input));
+  }
+  return Object.keys(template).reduce((tmp: any, key: string) => {
+    tmp[key] = processTemplate(template[key], match, input);
+    return tmp;
+  }, {});
+};
+
+export const stringTemplateAgent: AgentFunction<
+  {
+    template: StringTemplateObject;
+  },
+  StringTemplateObject,
+  string
+> = async ({ params, inputs, namedInputs }) => {
+  console.log(inputs, namedInputs);
+  if (params.template === undefined) {
+    console.warn("warning: stringTemplateAgent no template");
+  }
+  if (inputs && inputs.length > 0) {
+    return inputs.reduce((template, input, index) => {
+      return processTemplate(template, "${" + index + "}", input);
+    }, params.template);
+  }
+  return Object.keys(namedInputs).reduce((template, key) => {
+    return processTemplate(template, "${" + key + "}", namedInputs[key]);
+  }, params.template);
+};
+
+const sampleInput = ["hello", "test"];
+const sampleNamedInput = { message1: "hello", message2: "test" };
+
+// for test and document
+const stringTemplateAgentInfo: AgentFunctionInfo = {
+  name: "stringTemplateAgent",
+  agent: stringTemplateAgent,
+  mock: stringTemplateAgent,
+  samples: [
+    {
+      inputs: sampleInput,
+      params: { template: "${0}: ${1}" },
+      result: "hello: test",
+    },
+    {
+      inputs: sampleInput,
+      params: { template: ["${0}: ${1}", "${1}: ${0}"] },
+      result: ["hello: test", "test: hello"],
+    },
+    {
+      inputs: sampleInput,
+      params: { template: { apple: "${0}", lemon: "${1}" } },
+      result: { apple: "hello", lemon: "test" },
+    },
+    {
+      inputs: sampleInput,
+      params: { template: [{ apple: "${0}", lemon: "${1}" }] },
+      result: [{ apple: "hello", lemon: "test" }],
+    },
+    {
+      inputs: sampleInput,
+      params: { template: { apple: "${0}", lemon: ["${1}"] } },
+      result: { apple: "hello", lemon: ["test"] },
+    },
+    // named
+    {
+      inputs: sampleNamedInput,
+      params: { template: "${message1}: ${message2}" },
+      result: "hello: test",
+    },
+    {
+      inputs: sampleNamedInput,
+      params: { template: ["${message1}: ${message2}", "${message2}: ${message1}"] },
+      result: ["hello: test", "test: hello"],
+    },
+    {
+      inputs: sampleNamedInput,
+      params: { template: { apple: "${message1}", lemon: "${message2}" } },
+      result: { apple: "hello", lemon: "test" },
+    },
+    {
+      inputs: sampleNamedInput,
+      params: { template: [{ apple: "${message1}", lemon: "${message2}" }] },
+      result: [{ apple: "hello", lemon: "test" }],
+    },
+    {
+      inputs: sampleNamedInput,
+      params: { template: { apple: "${message1}", lemon: ["${message2}"] } },
+      result: { apple: "hello", lemon: ["test"] },
+    },
+  ],
+  description: "Template agent",
+  category: ["string"],
+  author: "Satoshi Nakajima",
+  repository: "https://github.com/receptron/graphai",
+  license: "MIT",
+};
+export default stringTemplateAgentInfo;
